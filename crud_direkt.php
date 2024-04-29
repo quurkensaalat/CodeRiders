d3baa94d1d771142cad0a7307e583a58856f3747
fabianbvb11 <fabian.keller@stud.fhgr.ch>
2024-04-29 13:18:06 +0200

    test

--- crud_direkt.php
+++ crud_direkt.php
@@ -3,6 +3,14 @@ require_once 'config.php'; // DB-Verbindungsdaten aus externer Datei laden
 
 // 0. Datenbankverbindung mit PDO herstellen
 
+// 2. Einfügen eines neuen Datensatzes
+
+// 3. Lesen eines Datensatzes mit id
+
+// 4. Lesen aller Datensätze, die den String $string in firstname, lastname oder email enthalten
+
+// 5. Delete mit email 
+
 try {
 
   $pdo = new PDO($dsn, $db_user, $db_pass, $options);
